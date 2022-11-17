fee338b41ccf3f49c84158130f5b633cfb2c05af
carlosribas <caduribas@gmail.com>
2022-11-17 18:43:46 +0000

    Add fields used by the annotations table

--- backend/sequence/api/accession/serializers.py
+++ backend/sequence/api/accession/serializers.py
@@ -1,10 +1,26 @@
 from rest_framework import serializers
 
-from ...models import Accession
 
-
-class AccessionSerializer(serializers.ModelSerializer):
+class AccessionSerializer(serializers.Serializer):
     """Serializer class for individual cross-references"""
-    class Meta:
-        model = Accession
-        fields = "__all__"
+    id = serializers.ReadOnlyField(source="accession")
+    parent_ac = serializers.ReadOnlyField()
+    seq_version = serializers.ReadOnlyField()
+    feature_start = serializers.ReadOnlyField()
+    feature_end = serializers.ReadOnlyField()
+    description = serializers.ReadOnlyField()
+    external_id = serializers.ReadOnlyField()
+    optional_id = serializers.ReadOnlyField()
+    gene = serializers.ReadOnlyField()
+    inference = serializers.ReadOnlyField()
+    locus_tag = serializers.ReadOnlyField()
+    note = serializers.ReadOnlyField(source="get_json_object_from_note")
+    standard_name = serializers.ReadOnlyField()
+    external_id_without_version = serializers.ReadOnlyField(
+        source="get_external_id_without_version"
+    )
+    pdb_entity_id = serializers.ReadOnlyField(source="get_pdb_entity_id")
+    ena_url = serializers.ReadOnlyField(source="get_ena_url")
+    ensembl_species_url = serializers.ReadOnlyField(
+        source="get_ensembl_species_url"
+    )
