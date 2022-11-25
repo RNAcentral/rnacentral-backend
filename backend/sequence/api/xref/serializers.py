d27b063d66e3b7a4df412c0e941f309e5366fbeb
carlosribas <caduribas@gmail.com>
2022-11-25 12:25:49 +0000

    Use SerializerMethodField to update field value

--- backend/sequence/api/xref/serializers.py
+++ backend/sequence/api/xref/serializers.py
@@ -3,9 +3,12 @@ from ..accession.serializers import AccessionSerializer
 
 
 class XrefSerializer(serializers.Serializer):
-    database = serializers.ReadOnlyField(source="db")
-    is_active = serializers.ReadOnlyField(source="deleted")
-    first_seen = serializers.ReadOnlyField(source="created")
-    last_seen = serializers.ReadOnlyField(source="last")
+    database = serializers.ReadOnlyField(source="db.display_name")
+    is_active = serializers.SerializerMethodField(method_name="get_deleted")
+    first_seen = serializers.ReadOnlyField(source="created.release_date")
+    last_seen = serializers.ReadOnlyField(source="last.release_date")
     taxid = serializers.ReadOnlyField()
     accession = AccessionSerializer()
+
+    def get_deleted(self, obj):
+        return True if obj.deleted == "N" else False
