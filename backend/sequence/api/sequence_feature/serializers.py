9380cf1312e3d7e84e126beb51d08a782fd2a3fe
carlosribas <caduribas@gmail.com>
2022-11-25 14:48:21 +0000

    Use serializers.ReadOnlyField

--- backend/sequence/api/sequence_feature/serializers.py
+++ backend/sequence/api/sequence_feature/serializers.py
@@ -2,10 +2,10 @@ from rest_framework import serializers
 
 
 class SequenceFeatureSerializer(serializers.Serializer):
-    feature_name = serializers.CharField(max_length=50, read_only=True)
+    feature_name = serializers.ReadOnlyField()
     metadata = serializers.JSONField(read_only=True)
-    start = serializers.IntegerField(read_only=True)
-    stop = serializers.IntegerField(read_only=True)
-    taxid = serializers.IntegerField(read_only=True)
-    accession = serializers.StringRelatedField(read_only=True)
-    upi = serializers.StringRelatedField(read_only=True)
+    start = serializers.ReadOnlyField()
+    stop = serializers.ReadOnlyField()
+    taxid = serializers.ReadOnlyField()
+    accession = serializers.StringRelatedField(many=True)
+    upi = serializers.StringRelatedField()
