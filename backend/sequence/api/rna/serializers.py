1aa1a7b5ef5075977a13baa0aa33f1537186fc2a
carlosribas <caduribas@gmail.com>
2022-11-27 11:07:46 +0000

    Use serializers.ReadOnlyField

--- backend/sequence/api/rna/serializers.py
+++ backend/sequence/api/rna/serializers.py
@@ -3,7 +3,7 @@ from rest_framework import serializers
 
 class RnaSerializer(serializers.Serializer):
     """Serializer class for a unique RNAcentral sequence"""
-    rnacentral_id = serializers.CharField(source="upi", max_length=13, read_only=True)
-    sequence = serializers.CharField(source="get_sequence", read_only=True)
-    length = serializers.IntegerField(read_only=True)
-    md5 = serializers.CharField(max_length=32, read_only=True)
+    rnacentral_id = serializers.ReadOnlyField(source="upi")
+    sequence = serializers.ReadOnlyField(source="get_sequence")
+    length = serializers.ReadOnlyField()
+    md5 = serializers.ReadOnlyField()
