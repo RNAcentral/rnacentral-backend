599ca98ba48d84b6624ed0d8b590f6a5684448e6
carlosribas <caduribas@gmail.com>
2023-12-12 13:20:27 +0000

    Remove response_model_by_alias

--- backend/main.py
+++ backend/main.py
@@ -20,7 +20,7 @@ def get_db():
 db_dependency = Annotated[Session, Depends(get_db)]
 
 
-@app.get("/rna/{id}", response_model=schemas.RnaPrecomputed, response_model_by_alias=False)
+@app.get("/rna/{id}", response_model=schemas.RnaPrecomputed)
 async def read_rna_precomputed(id: str, db: db_dependency):
     rna_precomputed = db.query(models.RnaPrecomputed).filter(models.RnaPrecomputed.id == id).first()
     if not rna_precomputed:
@@ -28,7 +28,7 @@ async def read_rna_precomputed(id: str, db: db_dependency):
     return rna_precomputed
 
 
-@app.get("/rna/{upi}/xrefs", response_model=list[schemas.Xref], response_model_by_alias=False)
+@app.get("/rna/{upi}/xrefs", response_model=list[schemas.Xref])
 async def read_rna_precomputed(upi: str, db: db_dependency, skip: int = 0, limit: int = 10):
     xrefs = db.query(models.Xref).filter(models.Xref.upi == upi).offset(skip).limit(limit).all()
     if not xrefs:
