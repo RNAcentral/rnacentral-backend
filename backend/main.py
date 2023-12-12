79651657d40f3257db8630c76138b53dcb766cca
carlosribas <caduribas@gmail.com>
2023-12-12 16:18:03 +0000

    Update xref endpoint

--- backend/main.py
+++ backend/main.py
@@ -21,16 +21,26 @@ db_dependency = Annotated[Session, Depends(get_db)]
 
 
 @app.get("/rna/{id}", response_model=schemas.RnaPrecomputed)
-async def read_rna_precomputed(id: str, db: db_dependency):
-    rna_precomputed = db.query(models.RnaPrecomputed).filter(models.RnaPrecomputed.id == id).first()
-    if not rna_precomputed:
+async def rna_precomputed(id: str, db: db_dependency):
+    """
+    Unique RNAcentral Sequence.
+    """
+    rna = db.query(models.RnaPrecomputed).filter(models.RnaPrecomputed.id == id).first()
+    if not rna:
         raise HTTPException(status_code=404, detail="RNA not found")
-    return rna_precomputed
+    return rna
 
 
-@app.get("/rna/{upi}/xrefs", response_model=list[schemas.Xref])
-async def read_rna_precomputed(upi: str, db: db_dependency, skip: int = 0, limit: int = 10):
-    xrefs = db.query(models.Xref).filter(models.Xref.upi == upi).offset(skip).limit(limit).all()
-    if not xrefs:
-        raise HTTPException(status_code=404, detail=f"No xref found for {upi}")
-    return xrefs
+@app.get("/rna/{upi}/xrefs/{taxid}", response_model=list[schemas.Xref])
+async def xrefs(upi: str, taxid: int, db: db_dependency, skip: int = 0, limit: int = 10):
+    """
+    List of cross-references for a particular RNA sequence in a specific species
+    """
+    results = db.query(models.Xref).filter(
+        models.Xref.upi == upi, models.Xref.taxid == taxid
+    ).offset(skip).limit(limit).all()
+
+    if not results:
+        raise HTTPException(status_code=404, detail=f"No xref found for {upi}_{taxid}")
+
+    return results
