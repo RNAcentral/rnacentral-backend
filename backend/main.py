b2e9042417fff8a3d14754d4c6cf84dd1e5bc0f9
carlosribas <caduribas@gmail.com>
2024-01-06 10:48:25 +0000

    Show data from rna and rnc_rna_precomputed tables

--- backend/main.py
+++ backend/main.py
@@ -25,10 +25,29 @@ async def rna_precomputed(id: str, db: db_dependency):
     """
     Unique RNAcentral Sequence.
     """
-    rna = db.query(models.RnaPrecomputed).filter(models.RnaPrecomputed.id == id).first()
-    if not rna:
+    data = db.query(models.RnaPrecomputed).filter(models.RnaPrecomputed.id == id).first()
+    if not data:
         raise HTTPException(status_code=404, detail="RNA not found")
-    return rna
+
+    response = schemas.RnaPrecomputed(
+        id=data.id,
+        taxid=data.taxid,
+        description=data.description,
+        upi=data.upi,
+        rna_type=data.rna_type,
+        update_date=data.update_date,
+        has_coordinates=data.has_coordinates,
+        databases=data.databases,
+        is_active=data.is_active,
+        last_release=data.last_release,
+        short_description=data.short_description,
+        length=data.rna.length,
+        seq_short=data.rna.seq_short,
+        seq_long=data.rna.seq_long,
+        md5=data.rna.md5
+    )
+
+    return response
 
 
 @app.get("/rna/{upi}/xrefs/{taxid}", response_model=list[schemas.Xref])
