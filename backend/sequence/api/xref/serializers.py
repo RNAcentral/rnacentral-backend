eea4fc402df16b7f49a865600733007f3e890984
carlosribas <caduribas@gmail.com>
2024-02-14 12:07:11 +0000

    Add mirbase_precursor

--- backend/sequence/api/xref/serializers.py
+++ backend/sequence/api/xref/serializers.py
@@ -2,6 +2,7 @@ from drf_spectacular.types import OpenApiTypes
 from drf_spectacular.utils import extend_schema_field
 from rest_framework import serializers
 from ..accession.serializers import AccessionSerializer
+from ...models.related_sequences import RelatedSequence
 
 
 class XrefSerializer(serializers.Serializer):
@@ -15,7 +16,19 @@ class XrefSerializer(serializers.Serializer):
     )
     taxid = serializers.IntegerField()
     accession = AccessionSerializer()
+    mirbase_precursor = serializers.SerializerMethodField(
+        method_name="get_mirbase_precursor"
+    )
 
     @extend_schema_field(OpenApiTypes.STR)
     def get_deleted(self, obj):
         return True if obj.deleted == "N" else False
+
+    def get_mirbase_precursor(self, obj):
+        if obj.db_id != 4:  # 4 = MIRBASE
+            return None
+
+        related_sequence = RelatedSequence.objects.filter(
+            source_accession=obj.accession_id, relationship_type="precursor"
+        ).first()
+        return related_sequence.target_urs_taxid_id if related_sequence else None
